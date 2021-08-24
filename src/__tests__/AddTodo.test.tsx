e6165ac61281ca59bbb3d12e50e99b8cb363f8ae
LG <gl717@outlook.com>
2021-08-24 21:35:01 +0800

    test: add ui test for AddTodo

---
+++ src/__tests__/AddTodo.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { AddTodo } from "../components";
+
+describe("AddTodo Test", () => {
+  test("should render input", () => {
+    render(<AddTodo onAddTodo={() => {}} />);
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("placeholder", "please add todo");
+  });
+
+  test("should type value for input", () => {
+    render(<AddTodo onAddTodo={() => {}} />);
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "todo1");
+
+    expect(input).toHaveValue("todo1");
+  });
+});
