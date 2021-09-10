2f3d72385cc1f6cedb24f10f87a6a4bf50c4075d
LG <gl717@outlook.com>
2021-09-10 16:15:45 +0800

    chore: temp

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Todo } from "../../data";
 import { TodosFilter } from "../../enum";
 
@@ -12,11 +12,15 @@ interface Props {
 
 const filterActiveTodos = (todos: Todo[]) => todos.filter((todo) => todo.complete === false);
 const filterCompleteTodos = (todos: Todo[]) => todos.filter((todo) => todo.complete === true);
-const filterAllTodos = (todos: Todo[]) => todos.filter(() => true);
 
 const TodoList: React.FC<Props> = ({ todos, todosFilter, onTodoChange }) => {
-  let viewTodos: Todo[] = [];
+  // const [viewTodos, setViewTodos] = useState<Todo[]>(todos);
 
+  // useEffect(() => {
+  //   setViewTodos(todos);
+  //   console.log("useEffect viewtodos", viewTodos);
+  // }, [todos]);
+  let viewTodos: Todo[] = [];
   switch (todosFilter) {
     case TodosFilter.Active:
       viewTodos = filterActiveTodos(todos);
@@ -25,12 +29,32 @@ const TodoList: React.FC<Props> = ({ todos, todosFilter, onTodoChange }) => {
       viewTodos = filterCompleteTodos(todos);
       break;
     case TodosFilter.All:
-      viewTodos = filterAllTodos(todos);
+      console.log("All");
+      viewTodos = todos;
+
       break;
   }
+  // useEffect(() => {
+  //   console.log("todosFilter", todosFilter);
+  //   switch (todosFilter) {
+  //     case TodosFilter.Active:
+  //       console.log("active");
+  //       setViewTodos(filterActiveTodos(todos));
+  //       break;
+  //     case TodosFilter.Complete:
+  //       console.log("Complete");
+  //       setViewTodos(filterCompleteTodos(todos));
+  //       break;
+  //     case TodosFilter.All:
+  //       console.log("All");
+  //       setViewTodos(todos);
+  //       break;
+  //   }
+  // }, [todosFilter]);
 
   return (
     <ul className="todo-list">
+      {console.log(JSON.stringify(viewTodos))}
       {viewTodos.map((todo: Todo, index: number) => (
         <li className={`todo-item ${todo.complete ? "complete" : ""}`} key={todo.id}>
           <input
